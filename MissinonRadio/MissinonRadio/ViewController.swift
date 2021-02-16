656fd8758bd28a9bf0f22fac3ce3f484b99b3071
kenshihoo <redwing.ironrange@gmail.com>
2021-02-16 23:16:35 +0900

    plistの設定

---
+++ MissinonRadio/MissinonRadio/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MissinonRadio
+//
+//  Created by Kenshiro on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
