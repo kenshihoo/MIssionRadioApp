95f3b39afb53ae075495365f22a1f098dd5da49b
kenshihoo <redwing.ironrange@gmail.com>
2021-02-27 23:38:39 +0900

    音声を保存するファイルを設定

--- MissinonRadio/MissinonRadio/RecordAudio.swift
+++ MissinonRadio/MissinonRadio/RecordAudio.swift
@@ -8,11 +8,7 @@
 import UIKit
 import AVFoundation
 
-class AudioEditorManager {
-    
-}
-
-class RecordAudio: UIViewController{
+class RecordAudio: UIViewController,FileManagerDelegate{
     let session = AVAudioSession.sharedInstance()
     
     override func viewDidLoad() {
@@ -26,6 +22,21 @@ class RecordAudio: UIViewController{
         }
         //録音と再生の両方を可能にする
         try? session.setCategory(.playAndRecord)
+        
+        //ファイルの設定
+        do {
+            let fileManager = FileManager.default
+            let docs = try fileManager.url(for: .documentDirectory,
+                                           in: .userDomainMask,
+                                           appropriateFor: nil, create: false)
+            let path = docs.appendingPathComponent("ecord.m4a")
+            let audio = "音声データ".data(using: .utf8)!
+
+            fileManager.createFile(atPath: path.path,
+                                   contents: audio, attributes: nil)
+        } catch {
+            print(error)
+        }
     }
 }
 
