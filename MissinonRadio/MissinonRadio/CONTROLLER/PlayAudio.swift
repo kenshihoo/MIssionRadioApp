f7b6e4e289df72ecc8153e91484f21908a21f1fe
kenshihoo <redwing.ironrange@gmail.com>
2021-03-14 19:29:27 +0900

    tableの設定追加

--- MissinonRadio/MissinonRadio/CONTROLLER/PlayAudio.swift
+++ MissinonRadio/MissinonRadio/CONTROLLER/PlayAudio.swift
@@ -10,10 +10,12 @@ import UIKit
 class PlayAudio:  UIViewController,UITableViewDelegate,UITableViewDataSource{
     
     @IBOutlet weak var audioList: UITableView!
+    
     @IBOutlet weak var recordModeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        audioList.dataSource = self
+        audioList.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
