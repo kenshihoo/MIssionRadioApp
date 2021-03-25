043fcea8e1fa683660af728dd35d09b0d6c0df7a
kenshihoo <redwing.ironrange@gmail.com>
2021-03-25 23:21:20 +0900

    tableViewCellの作成

---
+++ MissinonRadio/MissinonRadio/CONTROLLER/AudioListCell.swift
@@ -0,0 +1,26 @@
+//
+//  AudioListCell.swift
+//  MissinonRadio
+//
+//  Created by Kenshiro on 2021/03/25.
+//
+
+import UIKit
+
+class AudioListCell: UITableViewCell {
+    
+    @IBOutlet weak var audioNameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
